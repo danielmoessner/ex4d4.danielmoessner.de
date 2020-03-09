bbd21127f7fe1d6741e0e74808294e8702cd62f0
dmoetech <github@dmoe.tech>
2020-03-09 22:03:55 +0100

    removed unnecessary forms

--- apps/postforms/forms.py
+++ apps/postforms/forms.py
@@ -1,27 +1,6 @@
 from django import forms
 
 
-class KostenvoranschlagForm(forms.Form):
-    anrede = forms.ChoiceField(choices=(('Herr', 'Herr'), ('Frau', 'Frau')))
-    vorname = forms.CharField()
-    nachname = forms.CharField()
-    email = forms.EmailField()
-    telefon = forms.CharField()
-    bild1 = forms.ImageField(required=True)
-    bild2 = forms.ImageField(required=False)
-    bild3 = forms.ImageField(required=False)
-    bild4 = forms.ImageField(required=False)
-    bild5 = forms.ImageField(required=False)
-    bild6 = forms.ImageField(required=False)
-
-
-class KontaktForm(forms.Form):
-    name = forms.CharField()
-    email = forms.CharField()
-    website = forms.CharField(required=False)
-    message = forms.CharField(required=False)
-
-
 class PzmodeForm(forms.Form):
     name = forms.CharField()
     email = forms.CharField(required=False)
